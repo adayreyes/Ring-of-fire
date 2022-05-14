d4d8f62698f5272dacb056eb49d597e0fedbe1c0
adayreyes <adayreyes4a@gmail.com>
2022-05-14 20:56:05 +0200

    firebase added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,11 @@ import {MatInputModule} from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import {MatCardModule} from '@angular/material/card';
 import { InfoCardComponent } from './info-card/info-card.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 
 
@@ -38,6 +43,10 @@ import { InfoCardComponent } from './info-card/info-card.component';
     FormsModule,
     MatIconModule,
     MatDialogModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'ring-of-fire-3f187',
+    appId: '1:148108101635:web:a8edf2d42efc6d9852e636',
+    storageBucket: 'ring-of-fire-3f187.appspot.com',
+    apiKey: 'AIzaSyAOhDlbhzxWYtF0abOfsZI9x38doBTPUiw',
+    authDomain: 'ring-of-fire-3f187.firebaseapp.com',
+    messagingSenderId: '148108101635',
+    measurementId: 'G-C92F36Z16Q',
+  },
   production: true
 };
